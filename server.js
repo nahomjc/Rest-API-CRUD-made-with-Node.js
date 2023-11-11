4e57663247f51a592f27fde889337b3984d6d640
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-11-11 23:19:52 +0300

    feat: added delete API, fix: minor bug on post

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ const Product=require('./models/ProductModel')
 const app = express ()
 
 
-//routes
-app.get('/',(req ,res)=>{
-   res.send('hello Node APi') 
-})
+
 
 app.use(express.json())//r
 app.use(express.urlencoded({extended:false}))//Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
 app.get('/blog',(req,res)=>{
     res.send('hello Blog My is new')
 })
-
+//routes
+app.get('/',(req ,res)=>{
+    res.send('hello Node APi') 
+ })
+ 
 //get all Products
 app.get('/products',async(req,res)=>{
 
@@ -53,19 +54,34 @@ app.put('/product/:id',async(req,res)=>{
 res.status(500).json({message: error.message})
     }
 })
-// add a product 
-app.post('/product',async(req,res)=>{
-try{
-const product= await Product.create(req.body)
-res.status(200).json(product)
-}catch(error){
-console.log(error.message);
-res.status(500).json({message:error.message})
-}
-    
-    console.log(req.body,'response')
-    res.send(req.body)
+
+// delet a product 
+app.delete('/product/:id',async(req,res)=>{
+   
+    try{
+        const {id}=req.params
+        const product = await Product.findByIdAndDelete(id)
+        if(!product){
+            res.status(404).json({message: "Product not found"})
+            
+        }
+        res.status(200).json({ message: "Product deleted", product: product });
+        console.log(product,'successfully deleted')
+    }catch(error){
+
+res.status(500).json()
+    }
 })
+// add a product 
+app.post('/product', async (req, res) => {
+    const newProduct = new Product(req.body);
+    try {
+      const savedProduct = await newProduct.save();
+      res.status(201).json(savedProduct);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  });
 app.listen(3000, ()=>{
     console.log('Node API app is running on port 3000')
 })
