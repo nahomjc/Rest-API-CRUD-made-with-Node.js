f85dfd21289e0da7bc1e69ae1ba9bcaa184c5144
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-11-11 15:22:59 +0300

    feat: added nodemon ,mongodb

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express =require('express')
+const mongoose =require('mongoose')
 const app = express ()
 
+//routes
+app.get('/',(req ,res)=>{
+   res.send('hello Node APi') 
+})
+
+
+app.get('/blog',(req,res)=>{
+    res.send('hello Blog My is new')
+})
 app.listen(3000, ()=>{
-    console.log('Node API app is running on port 300')
+    console.log('Node API app is running on port 3000')
+})
+mongoose.connect('mongodb+srv://admin:asland2125@cluster0.daq8ybp.mongodb.net/Node-API?retryWrites=true&w=majority').then(()=>{
+    console.log('connected to MongoDB server');
+}).catch((error)=>{
+    console.log(error,'error connecting to MongoDB server')
 })
