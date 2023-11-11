6354962dbebb8537bc992338b4d83bfd33ad9b06
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-11-11 21:03:42 +0300

    feat:adding and getting products API

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 const express =require('express')
 const mongoose =require('mongoose')
+const Product=require('./models/ProductModel')
 const app = express ()
+
 
 //routes
 app.get('/',(req ,res)=>{
    res.send('hello Node APi') 
 })
 
-
+app.use(express.json())
 app.get('/blog',(req,res)=>{
     res.send('hello Blog My is new')
+})
+app.get('/products',async(req,res)=>{
+
+    try{
+const products= await Product.find({})
+res.status(200).json(products)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+
+    }
+})
+app.post('/product',async(req,res)=>{
+try{
+const product= await Product.create(req.body)
+res.status(200).json(product)
+}catch(error){
+console.log(error.message);
+res.status(500).json({message:error.message})
+}
+    
+    console.log(req.body,'response')
+    res.send(req.body)
 })
 app.listen(3000, ()=>{
     console.log('Node API app is running on port 3000')
